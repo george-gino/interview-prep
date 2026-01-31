55e327dce8a8e597759f6a7aa669de56dce30ce3
George Gino <georgejgino@gmail.com>
2026-01-30 21:13:34 -0700

    Fix white space on scroll: add dark background to html and body elements

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className="bg-gray-900">
+      <body className="bg-gray-900">{children}</body>
     </html>
   );
 }
